9b30bf573659f4b05b5843d762cb53d791cd7354
jiyoon <rlawldbs9128@gmail.com>
2025-03-28 02:53:54 +0900

    feat: Swagger에 JWT 인증 기능 추가 - Swagger UI에 Authorize 버튼 추가 - JWT 토큰 입력 시 인증 헤더 자동 적용

--- src/main/java/com/example/career/global/config/SwaggerConfig.java
+++ src/main/java/com/example/career/global/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package com.example.career.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT";
 
     @Bean
     public OpenAPI openAPI() {
@@ -22,6 +27,14 @@ public class SwaggerConfig {
                                 .email("rlawldbs9128@gmail.com"))
                         .license(new License()
                                 .name("Career License")
-                                .url("배포 주소 추가 예정")));
+                                .url("배포 주소 추가 예정")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
